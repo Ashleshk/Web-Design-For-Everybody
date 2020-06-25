da68886166ea5f26ac113cf7d536e0cb3263f8d9
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-06-25 12:55:59 +0530

    Home Transitons

---
+++ Course-2 Introduction to CSS3/Week-03/JS-Array Average.js
@@ -0,0 +1,7 @@
+var grades= [];
+
+var sum = 0;
+for (index = 0; index < grades.length; index++){
+		sum += grades[index];
+}
+document.write(sum/grades.length);
---
+++ Course-2 Introduction to CSS3/Week-03/JS-Inefiicient Code.js
@@ -0,0 +1,12 @@
+/*var name = prompt("What is your name?")*/
+
+if (name.length != 0 && name == "Colleen")
+      document.write("What a beautiful name");
+
+if (name.length != 0 && name != "Colleen")
+      document.write("Hello " +  name);
+
+if (name.length == 0)
+   document.write("Feeling shy?");
+
+/*Every conditional is checked instead of just one*/
