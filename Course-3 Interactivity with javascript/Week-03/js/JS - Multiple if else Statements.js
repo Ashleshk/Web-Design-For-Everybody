c426d5ecbff206e1c3dec9ac70d3276fd99f103a
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-06-28 11:12:28 +0530

    Change Style JS File

---
+++ Course-3 Interactivity with javascript/Week-03/js/JS - Multiple if else Statements.js
@@ -0,0 +1,12 @@
+var score = 97;  /*Replace with prompt*/
+
+if (score>90)
+  document.write("A");
+else if (score>80)
+  document.write("B");
+else if (score>70)
+  document.write("C");
+else if (score>60)
+  document.write("D");
+else
+  document.write("You failed");
---
+++ Course-3 Interactivity with javascript/Week-03/js/changeStyleA.js
@@ -0,0 +1,15 @@
+function closeMe(){
+    // Find the element
+         x=document.getElementById("demo");
+     //Option 1: Change the style attribute directly
+         x.style.display="none";
+
+  }
+
+function openMe(){
+    // Find the element
+         x=document.getElementById("demo");
+    //Option 1: Change the style attribute directly
+          x.style.display="block";
+
+  }
